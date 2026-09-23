d9822e7133c5973a268b9aae23dcca5d43718c32
ayushbhardwaj96 <ayush9508050914@gmail.com>
2026-09-23 19:15:30 +0530

    Update the changes

--- client/src/app/features/listingSlice.js
+++ client/src/app/features/listingSlice.js
@@ -3,6 +3,17 @@ import { dummyListings } from '../../assets/assets'
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import api from "../../configs/axios";
 
+// Get all public listings
+export const getAllPublicListing = createAsyncThunk("listing/getAllPublicListing", async () => {
+    try {
+        const { data } = await api.get("/api/listing/public");
+        return data;
+    } catch (error) {
+        console.log(error);
+        return [];
+    }
+});
+
 
 // Get all user listings
 export const getAllUserListing = createAsyncThunk("listing/getAllUserListing", async ({ getToken }) => {
